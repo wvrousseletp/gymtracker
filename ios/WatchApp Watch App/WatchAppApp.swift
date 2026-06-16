64ed28b90011ca8b679dea693ae4a312ed3e0416
Wéllerson Vicente Rousselet Porfírio <wellersonvicenterousseletporfirio@MacBook-Air-de-Wellerson.local>
2026-06-16 20:02:50 -0300

    feat: link watchOS companion target files in Xcode

---
+++ ios/WatchApp Watch App/WatchAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  WatchAppApp.swift
+//  WatchApp Watch App
+//
+//  Created by Wéllerson Vicente Rousselet Porfírio on 16/06/26.
+//
+
+import SwiftUI
+
+@main
+struct WatchApp_Watch_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            WorkoutSelectionView()
+        }
+    }
+}
