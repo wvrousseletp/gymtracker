b474d021e22c28ad58ce02283e7f0467368158eb
Wéllerson Vicente Rousselet Porfírio <wellersonvicenterousseletporfirio@MacBook-Air-de-Wellerson.local>
2026-07-30 08:51:52 -0300

    Fix gemini flash model name

--- lib/screens/main_navigation.dart
+++ lib/screens/main_navigation.dart
@@ -271,7 +271,7 @@ class _MainNavigationState extends State<MainNavigation> {
 
   void _checkWhatsNew() async {
     final prefs = await SharedPreferences.getInstance();
-    const currentBuild = 246;
+    const currentBuild = 247;
     final lastSeenBuild = prefs.getInt('last_seen_whats_new_build') ?? 0;
 
     if (lastSeenBuild < currentBuild) {
