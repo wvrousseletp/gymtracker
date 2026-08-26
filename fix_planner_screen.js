5be8c937fd633c77629e9f47a8122509907aee17
Wéllerson Vicente Rousselet Porfírio <wellersonvicenterousseletporfirio@MacBook-Air-de-Wellerson.local>
2026-08-26 16:20:45 -0300

    chore: remove script files

--- fix_planner_screen.js
+++
@@ -1,6 +0,0 @@
-const fs = require('fs');
-let content = fs.readFileSync('lib/screens/planner_screen.dart', 'utf8');
-
-content = content.replace('final routines = state.routines;', 'final library = state.library;\n    final routines = state.routines;');
-
-fs.writeFileSync('lib/screens/planner_screen.dart', content);
